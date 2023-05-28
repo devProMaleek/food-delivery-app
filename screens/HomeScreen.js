d84121cfee5c33c22e62fc40dfca4acd717ed600
Abdulmalik Adebayo <milikiadbay@gmail.com>
2023-05-28 12:29:26 +0100

    feat: Build the Honme screen

---
+++ screens/HomeScreen.js
@@ -0,0 +1,42 @@
+import { Text, TextInput, View, ScrollView } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import React from 'react';
+import * as Icon from 'react-native-feather';
+import { StatusBar } from 'expo-status-bar';
+import { themeColors } from '../themes';
+import Categories from '../components/Categories';
+import FeaturedRow from '../components/FeaturedRow';
+import { featured } from '../constants';
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView className="bg-white">
+      <StatusBar barStyle="dark-content" />
+      <View className="flex-row items-center px-4 pb-2 space-x-2">
+        <View className="flex-row items-center flex-1 p-3 border border-gray-300 rounded-full">
+          <Icon.Search height="25" width="25" stroke="gray" />
+          <TextInput placeholder="Restaurants" className="flex-1 ml-2" />
+          <View className="flex-row items-center pl-2 space-x-1 border-0 border-l-2 border-l-gray-300">
+            <Icon.MapPin height="20" width="20" stroke="gray" />
+            <Text className="text-gray-600">Lagos, Nigeria</Text>
+          </View>
+        </View>
+        <View style={{ backgroundColor: themeColors.bgColor(1) }} className="p-3 rounded-full">
+          <Icon.Sliders height="20" width="20" strokeWidth={2.5} stroke="white" />
+        </View>
+      </View>
+
+      <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{ paddingBottom: 20 }}>
+        <Categories />
+
+        <View className="mt-5">
+          {[featured, featured, featured].map((item, index) => {
+            return <FeaturedRow key={index} title={item.title} restaurants={item.restaurants} description={item.description} />;
+          })}
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
