4aef94c013fbd21963f523b3fede89ce24e2a053
Abdulmalik Adebayo <milikiadbay@gmail.com>
2023-05-29 23:58:04 +0100

    feat: Add new navigation screen to the project

--- navigation.js
+++ navigation.js
@@ -4,6 +4,9 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { View, Text } from 'react-native';
 import HomeScreen from './screens/HomeScreen';
 import RestaurantScreen from './screens/RestaurantScreen';
+import CartScreen from './screens/CartScreen';
+import OrderPrepareScreen from './screens/OrderPrepareScreen';
+import DeliveryScreen from './screens/DeliveryScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,6 +16,9 @@ const Navigation = () => {
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Restaurant" component={RestaurantScreen} />
+        <Stack.Screen name="Cart" options={{presentation: 'modal'}} component={CartScreen} />
+        <Stack.Screen name="OrderPreparing" options={{presentation: 'fullScreenModal'}} component={OrderPrepareScreen} />
+        <Stack.Screen name="Delivery" options={{presentation: 'fullScreenModal'}} component={DeliveryScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
