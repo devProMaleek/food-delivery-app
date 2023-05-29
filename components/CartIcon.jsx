7d5e5967fb6bf09a7fb7332247e89b3853f03936
Abdulmalik Adebayo <milikiadbay@gmail.com>
2023-05-29 23:57:42 +0100

    feat: Add the cart icon component

--- components/CartIcon.jsx
+++ components/CartIcon.jsx
@@ -1,12 +1,19 @@
 import { View, Text, TouchableOpacity } from 'react-native';
-import React from 'react';
+import React, { useCallback } from 'react';
 import { themeColors } from '../themes';
 import { formatCurrency } from '../utils';
+import { useNavigation } from '@react-navigation/native';
 
 const CartIcon = () => {
+  const navigation = useNavigation();
+  const handlePressNavigation = useCallback(() => {
+    navigation.navigate('Cart');
+  }, [navigation]);
+
   return (
     <View className="absolute z-50 w-full bottom-5">
       <TouchableOpacity
+        onPress={handlePressNavigation}
         style={{ backgroundColor: themeColors.bgColor(1) }}
         className="flex-row items-center justify-between p-4 py-3 mx-5 rounded-full shadow-lg"
       >
